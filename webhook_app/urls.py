8d7c6d54c358caefcd6b13164bd5eb25089e2e7c
raunakram <raunakron4@gmail.com>
2025-01-04 16:41:54 +0530

    update

--- webhook_app/urls.py
+++ webhook_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import s3_notifications
+from .views import s3_notifications, connection_view, index
 
 urlpatterns = [
     path('s3_notifications/', s3_notifications, name='s3_notifications'),
+    path('connect-s3/', connection_view, name='connection_view'),
+    path('rollerbar-check/', index, name='index-view'),
 ]
--- webhook_app/views.py
+++ webhook_app/views.py
@@ -1,5 +1,20 @@
 # your_app/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
 
 def s3_notifications(request):
     return render(request, 's3_notification.html')
+
+@csrf_exempt    
+def connection_view(request):
+    if request.method == 'POST':
+        aws_access_key_id = request.POST.get('aws_access_key_id')
+        aws_secret_access_key = request.POST.get('aws_secret_access_key')
+        region_name = request.POST.get('region_name')
+        # return render(request, 's3_notification.html')
+        return redirect("/s3_notifications/")
+    else:
+        return render(request, 'connection_form.html')
+        
